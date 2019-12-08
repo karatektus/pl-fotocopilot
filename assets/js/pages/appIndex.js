ac47ca51c8ce755d5c65cb91c1b730b96414c77c
Paul Lützner <mail@pluetzner.de>
2019-12-08 14:14:52 +0100

    update

--- assets/js/pages/appIndex.js
+++ assets/js/pages/appIndex.js
@@ -6,7 +6,7 @@ var appIndex = function () {
          * Init
          */
         init: function () {
-            window.fitText( document.getElementsByClassName("fittext"), 0.515 );
+            window.fitText( document.getElementsByClassName("fittext"), 0.53);
         }
     };
 }();
